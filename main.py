2ac4f0b10f16be4fcee6e1375cfef52a7f82a828
Simion George <george.simion204@gmail.com>
2025-03-18 15:38:44 +0200

    Added a basic, yet general keywords matching. Now looking to improve it a little bit

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import os
 import sys
 import numpy
 import pandas as pd
+import re
 
 # loading the initial data
 taxonomy = pd.read_excel('insurance_taxonomy.xlsx')
 insurance = pd.read_excel('ml_insurance_challenge.xlsx')
-
-print(insurance)
 
 # Made the initial plan:
 # Rule based + RL or auto learner 
@@ -19,3 +18,42 @@ print(insurance)
 # acesta poate adapta si regulile de baza (inca nu sunt sigur de asta)
 # dar pe scurt invata in timp ce le da label si analizeaza si nuantele 
 # iar datset ul nu e unul traditional ci invata pe masura ce analizeaza mai mult 
+
+
+labels = taxonomy['label'].dropna().unique()
+words_per_label = {}
+for l in labels:
+    l_words = re.split(r'\W+', l.lower())
+    l_words = [w for w in l_words if w]
+    words_per_label[l] = set(l_words)
+
+
+def match_words_from_company_to_label(company_text):
+    words = set(company_text.lower().split())
+    matched_labels = []
+
+    for l, l_words in words_per_label.items():
+        if l_words.issubset(words):
+            matched_labels.append(l)
+
+    return matched_labels
+
+def create_company_row(row):
+    fields = []
+
+    for col in ['description', 'business_tags', 'sector', 'category', 'niche']:
+        val = row.get(col)
+        if pd.notnull(val):
+            fields.append(str(val))
+
+    return ' '.join(fields)
+
+
+insurance['company_text'] = insurance.apply(create_company_row, axis=1)
+
+company_text = insurance.loc[1, 'company_text']
+matched = match_words_from_company_to_label(company_text)
+print(company_text)
+print(matched)
+print(len(matched))
+print(len(taxonomy))
